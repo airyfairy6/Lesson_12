cb3191b9a8aa089e8be1a035371b75f0f3e152a8
airyfairy6 <55426211+airyfairy6@users.noreply.github.com>
2022-07-07 08:14:09 +0200

    Update Triangel.java

--- 1313/src/main/Triangel.java
+++ 1313/src/main/Triangel.java
@@ -1,15 +1,49 @@
-package main;
+public class Triangle {
 
-public class Triangel {
-    public static void main(String[] args) {
+    private double a;
+    private double b;
+    private double c;
 
+    public Triangle() {
+        this.a = a;
+        this.b = b;
+        this.c=c;
+    }
+
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public double getC() {
+        return c;
+    }
+
+    public void setC(double c) {
+        this.c = c;
+    }
+
+    public  double geroneSquare (double a, double b, double c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            System.out.println("Invalid value");
+            return -1;
+        } else {
+            double p = (a + b + c) / 2;
+            return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
 
     }
 
-    public static double geroneSquare(double a, double b, double c) {
-        double p = (a + b + c) / 2;
-        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
 
-
-}
