6a60e6dd4b4b94ea3deb02f8caa93a7b5fb47a08
airyfairy6 <55426211+airyfairy6@users.noreply.github.com>
2022-07-07 08:13:34 +0200

    Update TriangleTest.java

--- 1313/src/test/java/TriangleTest.java
+++ 1313/src/test/java/TriangleTest.java
@@ -1,6 +1,6 @@
-import main.Triangel;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.platform.commons.logging.Logger;
@@ -11,87 +11,44 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TriangleTest {
-    private static Logger logger = LoggerFactory.getLogger(TriangleTest.class);
+public class TriangleTest1 {
+    private static Logger logger = LoggerFactory.getLogger(TriangleTest1.class);
+    private double s;
 
-    @BeforeClass
-    public static void beforeClass() {
-        System.setProperty("java.util.logging.config.file", ClassLoader.getSystemResource("logging.properties").getPath());
-    }
-
-    @BeforeAll
-    static void setup() {
-        System.out.println("BeforeAll executed");
-    }
 
     @BeforeEach
-    void setupThis() {
-        System.out.println("BeforeEach executed");
-    }
-
-    @Test
-    void test() {
-        System.out.println("Executed test method");
-    }
-
-
-    @Disabled
-    @Test
-    void additionTest() {
-
-    }
-
-    @DisplayName("Testing addition")
-    @Test
-    void addition2Test() {
-        System.out.println("Function named 1 executed");
+    void setUp() {
+        Triangle triangle = new Triangle();
     }
 
     @Test
     void lambdaExpressions() {
-        assertTrue(Stream.of(Triangel.geroneSquare(1, 2, 3))
+        Triangle triangle = new Triangle();
+
+        assertTrue(Stream.of(triangle.geroneSquare(1, 2, 3))
                         .mapToDouble(i -> i),
-                (Triangel.geroneSquare(1, 2, 3)) != 0, true, "Square should be greater than 0");
+                (triangle.geroneSquare(1, 2, 3)) != 0, true, "Square should be greater than 0");
         System.out.println("Square is greater than 0");
     }
 
     private void assertTrue(DoubleStream mapToDouble, boolean b, boolean b1, Object o) {
     }
 
-
-    @Tag("a")
-    @Test
-    void test1() {
-        System.out.println("Fulfilling a");
-    }
-
-    @DisplayName("Repeated Test")
-    @RepeatedTest(value = 2, name = "{displayName} -> {currentRepetition}")
-    void repeatedTest(RepetitionInfo repetitionInfo) {
-        System.out.println("Current iteration: " + repetitionInfo.getCurrentRepetition());
-
-    }
-
-
     @ParameterizedTest
     @ValueSource(doubles = {1, 2, 3})
     void isSquare(double s) {
+        Triangle triangle = new Triangle();
         double p = 3;
         s = Math.sqrt(p * (p - 1) * (p - 2) * (p - 3));
-        assertEquals(s, Triangel.geroneSquare(1, 2, 3));
+        assertEquals(s, triangle.geroneSquare(1, 2, 3));
         System.out.println("Formula works");
     }
 
-
     @AfterEach
-    void tearThis() {
-        System.out.println("AfterEach executed");
+    public void cleanUpEach() {
+        System.out.println("After Each cleanUpEach() method called");
     }
 
-    @AfterAll
-    static void tear() {
-        System.out.println("AfterAll executed");
-    }
 
 }
 
